07939f5a1b6b0dcb6939420171766da9653e2727
OdinYkt <kaktusaprel@gmail.com>
2023-04-22 22:02:35 +0300

    create table

--- sql_execute.py
+++ sql_execute.py
@@ -7,3 +7,26 @@ add_test = """INSERT INTO projects(
 'name', 'domen')
 VALUES ('test1', 'test2')
 """
+
+create_table = """CREATE TABLE IF NOT EXISTS projects (
+                      project_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT,
+                        description TEXT,
+                        domen TEXT,
+                        technology TEXT,
+                        metod TEXT,
+                        func_group TEXT,
+                        potencial_rate INTEGER,
+                        potencial_dec TEXT,
+                        market_rate INTEGER,
+                        market_mat TEXT,
+                        readiness_rate INTEGER,
+                        readiness TEXT,
+                        implementation TEXT,
+                        Benchmarking TEXT,
+                        Benchmarking_description TEXT,
+                        Benchmarking_company TEXT,
+                        name_project_gpn TEXT,
+                        description_project TEXT,
+                        name_project  TEXT
+                    )"""
--- sql_work.py
+++ sql_work.py
@@ -1,15 +1,43 @@
 import sqlite3
 import pandas as pd
+import openpyxl
 from sql_execute import *
 
-data = pd.read_excel('base.xlsm', sheet_name='ОСНОВНАЯ')
-data = data[['Наименование сценария', 'Домен']]
+# data = pd.read_excel('base.xlsm', sheet_name='ОСНОВНАЯ')
+# data = data[['Наименование сценария', 'Домен']]
+workbook = openpyxl.load_workbook('main_table.xlsx')
+worksheet = workbook['ОСНОВНАЯ']
 
 conn = sqlite3.connect('base.db')
 cur = conn.cursor()
 
-cur.execute(create_new)
-cur.execute(add_test)
+cur.execute(create_table)
+# cur.execute(add_test)
+
+for row in worksheet.iter_rows(min_row=2, values_only=True):
+    cur.execute (
+        ''' INSERT INTO projects ( name,
+                        description,
+                        domen,
+                        technology,
+                        metod,
+                        func_group,
+                        potencial_dec,
+                        potencial_rate,
+                        market_rate,
+                        market_mat,
+                        readiness_rate,
+                        readiness,
+                        implementation,
+                        Benchmarking,
+                        Benchmarking_description,
+                        Benchmarking_company,
+                        name_project_gpn,
+                        description_project,
+                        name_project)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', row)
+
 conn.commit()
 A = 1
 
